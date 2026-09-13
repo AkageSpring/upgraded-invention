40b8ccacd16b307b8207298ade155d3ed63541da
AkageSpring <akage.spring@gmail.com>
2026-09-13 18:13:12 +0300

    feat: add cli

--- Task List.md
+++ Task List.md
@@ -1,5 +1,5 @@
 # Task List
 
-- [ ] Пронумеровать строки в `metallic_colors_copy.csv`
-- [ ] Пронумеровать строки в `classic_colors.csv`
-- [ ] Спосоставить цвета и их значения
+- [x] Пронумеровать строки в `metallic_colors_copy.csv`
+- [x] Пронумеровать строки в `csv/classic_colors.csv`
+- [x] Спосоставить цвета и их значения
--- src/main/java/com/akage/Main.java
+++ src/main/java/com/akage/Main.java
@@ -1,54 +1,40 @@
 package com.akage;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVRecord;
-
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        ColorExtractor colorExtractor = new ColorExtractor();
-//        System.out.print(colorExtractor.getColor(args[0], Integer.parseInt(args[1])));
-        Reader in;
-        try {
-            in = new FileReader("C:\\Users\\Danila\\Desktop\\Java\\upgraded-invention\\metallic_colors_copy.csv");
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-        Iterable<CSVRecord> records;
-        CSVFormat myFormat = CSVFormat.DEFAULT.builder()
-                .setHeader()
-                .setSkipHeaderRecord(true)
-                .get();
-        try {
-            records = myFormat.parse(in);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        for (CSVRecord record : records) {
-            String name = record.get("name");
-            System.out.println(name);
+        ColorExtractor colorExtractor = new ColorExtractor();
+
+        System.out.println("Welcome!");
+        System.out.println("1. Extract color");
+        System.out.println("2. Get best color");
+        System.out.println("0. Quit");
+        System.out.print("> ");
+
+        Scanner scanner = new Scanner(System.in);
+        String choice = scanner.nextLine();
+
+        if (choice.equals("0")) {
+            System.exit(0);
         }
 
-        List<String> lines = null;
-        try {
-            lines = Files.readAllLines(Path.of("C:\\Users\\Danila\\Desktop\\Java\\upgraded-invention\\metallic_colors_copy.csv"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        if (choice.equals("1")) {
+            System.out.print("File path: > ");
+            String filePath = scanner.nextLine();
+            filePath = filePath.substring(1, filePath.length() - 1);
+            System.out.print("Color number: > ");
+            String colorNumber = scanner.nextLine();
+            System.out.println(colorExtractor.getColor(filePath, Integer.parseInt(colorNumber)));
         }
 
-        int lineNumber = 0;
-        for (String line : lines) {
-            line = line.replace("#", String.valueOf(lineNumber));
-            lineNumber++;
-            System.out.println(line);
+        if (choice.equals("2")) {
+            System.out.print("Your color: > ");
+            String userColor = scanner.nextLine();
+            System.out.print("Color number: > ");
+            String filePath = scanner.nextLine();
+            filePath = filePath.substring(1, filePath.length() - 1);
+            System.out.println(colorExtractor.getBestColor(userColor, filePath));
         }
     }
 }
